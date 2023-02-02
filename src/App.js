4f8f4ad5ebb9c6332d183be2c958a9c5048b4c2c
junaid-12junaid <junaidullakhan619@gmail.com>
2023-02-02 15:31:08 +0530

    Route with withRouter()

--- src/App.js
+++ src/App.js
@@ -2,11 +2,28 @@ import logo from './logo.svg';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 
+import {Switch, Route,Link } from 'react-router-dom';
+
+const HatsPage=(props)=>{
+  console.log(props)
+ return( 
+ <div>
+    
+    <h1>Hats Page</h1>
+  </div>
+ )
+}
+
 function App() {
   return (
     <div>
-     <HomePage/>
+  
+    <Switch>
+     <Route  exact path='/' component={HomePage} />
+     <Route  exact path='/shop/hats' component={HatsPage} />
+     </Switch>
     </div>
+    
   );
 }
 
