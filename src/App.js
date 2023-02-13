6256d5ca44ea2cc1ddf78aa9ee6da9cfb4dd42ee
junaid-12junaid <junaidullakhan619@gmail.com>
2023-02-13 11:42:22 +0530

    user action

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component.jsx';
-import {Switch, Route,Link } from 'react-router-dom';
+import {Switch, Route,Redirect } from 'react-router-dom';
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import { auth,createUserProfileDocument } from './firebase/firebase.utils';
@@ -53,7 +53,16 @@ class App extends React.Component {
       <Switch>
        <Route  exact path='/' component={HomePage} />
        <Route    path='/shop' component={ShopPage} />
-       <Route    path='/signin' component={SignInAndSignUpPage} />
+       <Route exact
+            path='/signin'
+            render={() =>
+              this.props.currentUser ? (
+                <Redirect to='/' />
+              ) : (
+                <SignInAndSignUpPage />
+              )
+            }
+          />
   
        </Switch>
       </div>
@@ -63,12 +72,15 @@ class App extends React.Component {
   
 }
 
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+});
 
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 });
 
 export default connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps
 )(App);
