500860c7a2d52cdfbe0871a441fa5225729f58c2
junaid-12junaid <junaidullakhan619@gmail.com>
2023-02-16 08:45:27 +0530

    Added the Session and Local Storage using redux-persist

--- src/index.js
+++ src/index.js
@@ -4,15 +4,18 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from 'react-router-dom'
-import store from './redux/store';
+import {store,persistor} from './redux/store';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
   <Provider store={store}>
     <BrowserRouter>
-    <App />
+    <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
 </BrowserRouter>
 </Provider>
   // </React.StrictMode>
